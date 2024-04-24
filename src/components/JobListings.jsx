da171416ab4388e5610ee0292089871ac5c7bd6d
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-04-24 10:42:48 -0500

    finished JobPage

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,9 +1,36 @@
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing'
-import jobs from '../jobs.json'
-
+import Spinner from './Spinner';
 
 const JobListings = ({ isHome = false }) => {
-    const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+    // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+
+    // [name of state, func to update state] = useState(default value of state)
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // useEffect() is a handy tool for doing stuff after your component
+    // shows up and cleaning up after it's gone. 
+    useEffect( () => {
+        const fetchJobs = async () => {
+            const apiURL = isHome
+            ? '/api/jobs?_limit=3'
+            : '/api/jobs';
+
+            try {
+                const res = await fetch(apiURL);
+                const data = await res.json();
+                setJobs(data);
+            } catch (error) {
+                console.log('Error fetching data', error);
+            } finally {
+                // finally block happens in any case
+                setLoading(false);
+            }
+        }; // ends fetchJobs()
+
+        fetchJobs();
+    }, []); // ends useEffect()
 
     return (
         <section className="bg-blue-50 px-4 py-10">
@@ -11,11 +38,16 @@ const JobListings = ({ isHome = false }) => {
                 <h2 className="text-3xl font-bold tex t-indigo-500 mb-6 text-center">
                     { isHome ? 'Recent Jobs' : 'Browse Jobs' }
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    { jobListings.map((job) => (
-                        < JobListing key={job.id} job={job}/>
+
+                { loading 
+                ? < Spinner loading={loading} />
+                : <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                    { jobs.map((job) => (
+                        < JobListing key={job.id} job={job} />
                     )) }                    
-                </div>
+                  </div>
+                }
+
             </div>
         </section>
     )
